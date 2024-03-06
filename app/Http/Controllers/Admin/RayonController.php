dbb09f3d1e85d76812ab82369b769bf02ee7bd95
Sardor Saidov <sardorjs17@gmail.com>
2024-03-06 15:15:06 +0500

    feature/empty_migrations_and_models_with_all_relations_seed_factories

---
+++ app/Http/Controllers/Admin/RayonController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreRayonRequest;
+use App\Http\Requests\UpdateRayonRequest;
+use App\Models\Rayon;
+
+class RayonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRayonRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rayon $rayon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rayon $rayon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRayonRequest $request, Rayon $rayon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rayon $rayon)
+    {
+        //
+    }
+}
